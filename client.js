ba0ee51e3529019001946f50bab74b4bab5c5cfd
Enzo Notario <enzo.notario@gmail.com>
2023-01-07 11:20:48 -0300

    feat: vavite ssr

---
+++ client.js
@@ -0,0 +1,41 @@
+import { createSSRApp, h } from "vue";
+import App from "./src/App.esvue";
+import rutas from './src/rutas.esjs';
+import axios from 'axios';
+
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
+
+async function render() {
+	const importer = rutas[window.location.pathname]
+
+	if (!importer) {
+		throw new Error(`No page found for ${window.location.pathname}`);
+	}
+
+	const Page = (await importer()).default;
+
+	const apiUrl = 'https://dolar-api-argentina.vercel.app/'
+	const dolares = (await axios.get(apiUrl + 'dolares')).data
+
+	const app = createSSRApp({
+		render() {
+			return h(
+				App,
+				{},
+				{
+					default() {
+						return h(Page, {
+							apiUrl,
+							dolares,
+						});
+					},
+				},
+			);
+		},
+	});
+
+	app.mount("#root");
+}
+
+render();
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import vue from '@vitejs/plugin-vue'
 import EsJS from '@es-js/vite-plugin-esjs'
 import EsVue from '@es-js/vite-plugin-esvue'
 import Unocss from 'unocss/vite'
-import viteSSR from 'vite-ssr/plugin.js'
+import vavite from 'vavite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    viteSSR(),
-
     vue({
       include: [/\.vue$/, /\.esvue$/],
     }),
@@ -23,6 +21,13 @@ export default defineConfig({
     Unocss({
       include: [/\.vue$/, /\.esvue$/],
     }),
+
+    // https://github.com/cyco130/vavite
+    vavite({
+			serverEntry: "/server.js",
+			serveClientAssetsInDev: true,
+			reloadOn: "static-deps-change",
+		}),
   ],
   resolve: {
     alias: {
@@ -43,4 +48,25 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  buildSteps: [
+		{
+			name: "client",
+			config: {
+				build: {
+					outDir: "dist/client",
+					manifest: true,
+					rollupOptions: { input: "client.js" },
+				},
+			},
+		},
+		{
+			name: "server",
+			config: {
+				build: {
+					ssr: true,
+					outDir: "dist/server",
+				},
+			},
+		},
+	]
 })
